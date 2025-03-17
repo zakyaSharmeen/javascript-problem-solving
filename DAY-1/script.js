a6bb2dfd99e5ce86a689f532b36d02c5133e4db0
zakya sharmeen <sakarim9124@gmail.com>
2025-03-17 21:48:11 +0530

    day-1

---
+++ DAY-1/script.js
@@ -0,0 +1,19 @@
+// let a = 5;
+// let b = 10;
+// let sum = a + b;
+
+// console.log(sum); // Output: 15
+
+let a = parseFloat(prompt("Enter first number:"));
+let b = parseFloat(prompt("Enter second number:"));
+let sum = a + b;
+
+console.log(`The sum is: ${sum}`);
+alert(`The sum is: ${sum}`);
+
+// do this afterwords
+// 1-Given an array [1, 2, 3, 4, 5], use forEach() to print each number.
+// 2-Use forEach() to find the sum of all elements in an array [10, 20, 30, 40].
+// 3-Given an array ["apple", "banana", "cherry"], use forEach() to print each fruit in uppercase.
+// 4-Use forEach() to count how many numbers in [5, 10, 15, 20, 25, 30] are greater than 15.
+// 5-Given an array ["a", "b", "c", "d"], use forEach() to create a string of all elements joined together.
